a55c9604d7457cd14aae421cc742bc68ceb18383
Doug <douglas@macOs.local>
2025-05-02 23:20:01 -0300

    Created constructor, getters and setters and method to fill fuel tank in class Car

--- src/ExercisesClass/Car.java
+++ src/ExercisesClass/Car.java
@@ -6,4 +6,40 @@ public class Car {
     String model;
     double fuelTankCapacity;
 
+    Car(){}
+
+    Car(String color, String model, double fuelTankCapacity){
+        this.color = color;
+        this.model = color;
+        this.fuelTankCapacity = fuelTankCapacity;
+    }
+
+    double fillTank(double fuelTankCapacity, double price){
+
+        return fuelTankCapacity * price;
+    }
+
+    String getColor() {
+        return color;
+    }
+
+    void setColor(String color) {
+        this.color = color;
+    }
+
+    String getModel() {
+        return model;
+    }
+
+    void setModel(String model) {
+        this.model = model;
+    }
+
+    double getFuelTankCapacity() {
+        return fuelTankCapacity;
+    }
+
+    void setFuelTankCapacity(double fuelTankCapacity) {
+        this.fuelTankCapacity = fuelTankCapacity;
+    }
 }
